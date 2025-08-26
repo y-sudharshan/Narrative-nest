709928fb5a22c13a8151d0c289b835b4e23647e2
y-sudharshan <sudharshanr2017@gmail.com>
2025-08-26 15:02:39 +0530

    middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (!token) {
+    return res.status(401).json({ error: "Not authorized, no token" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Not authorized, token failed" });
+  }
+};
+
+export const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).json({ error: "Admin access required" });
+  }
+};
